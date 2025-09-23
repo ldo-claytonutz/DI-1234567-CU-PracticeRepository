b4c48e6c3611431970e16e9d82916074622f6ac8
ldo <ldo@claytonutz.com>
2025-09-24 09:53:42 +1000

    create function to export df to excel

--- utils/export_utils.py
+++ utils/export_utils.py
@@ -9,3 +9,12 @@ from openpyxl import load_workbook
 class ExportUtils:
 
     # to export a SQL table to an Excel file.
+    @staticmethod
+    def save_df_to_excel(df: pd.DataFrame, file_name: PathLike, index=False, **pdkwargs) -> None:
+        """
+        df: Pandas dataframe to save
+        file_name: destination path to save
+        index: whether to include the pandas dataframe index in output (default is False)
+        pdkwargs: other kwargs to pass into the .to_excel() method
+        """
+        df.to_excel(file_name, index=index, **pdkwargs)
