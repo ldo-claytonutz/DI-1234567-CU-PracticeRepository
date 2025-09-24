14b78cc98d0d19d8ab9926ae858e47a56b434fc5
ldo <ldo@claytonutz.com>
2025-09-24 11:24:11 +1000

    update: find product with the highest quantity in a single order

--- SQL/analysis_test.sql
+++ SQL/analysis_test.sql
@@ -2,6 +2,7 @@ USE [zzz_ldo];
 GO
 
 --Request: Return all order details for orders handled by employee #5 where any individual product line in those orders has a quantity greater than 20 units.
+--Additional request: find product with the highest quantity in a single order 
 
 WITH orderID_Lookup AS (
     SELECT * 
@@ -13,3 +14,10 @@ FROM [dbo].[order_details] a
 where a.OrderID in (select DISTINCT OrderID from orderID_Lookup)
 and Quantity > 20
 ORDER BY Quantity DESC; 
+
+-- find product with the highest quantity in a single order
+SELECT od.*
+FROM dbo.order_details od
+JOIN dbo.orders o ON o.OrderID = od.OrderID
+WHERE od.Quantity = (SELECT MAX(Quantity) FROM dbo.order_details)
+ORDER BY od.Quantity DESC;
